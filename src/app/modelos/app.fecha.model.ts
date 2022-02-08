45e078ad4bd29cbfff02825d9152f69c0a2f23f7
Eghar <nahucarc2@gmail.com>
2022-02-07 23:59:37 -0300

    correccion funcion diasMaximos

--- src/app/modelos/app.fecha.model.ts
+++ src/app/modelos/app.fecha.model.ts
@@ -20,13 +20,13 @@ export class Fecha{
         const dias30:number[] = [4,6,9,11];
         const dias31:number[] = [1,3,5,7,8,10,12]
         if (dias30.includes(mes)){
-            return 31;
+            return 30;
         }
         else if (mes ===2){
             return 28;
         }
         else if(dias31.includes(mes)){
-            return 30;
+            return 31;
         }
         else{
             return 0;
